7ec980e98f53aeee0e93f93fe4215c90a5ec6a26
Ricardo Campos <ricardo.campos@encora.com>
2024-09-20 10:27:16 -0300

    test: add basic test case and setup coverage

---
+++ client/src/__test__/App.test.tsx
@@ -0,0 +1,13 @@
+import { test } from "vitest";
+import App from "../App";
+import { render } from "@testing-library/react";
+import AuthContext from "../context/AuthContext";
+import authContextMock from "./__mocks__/authContextMock";
+
+test('Renders the app', () => {
+  render(
+    <AuthContext.Provider value={authContextMock}>
+      <App />
+    </AuthContext.Provider>
+  );
+});
