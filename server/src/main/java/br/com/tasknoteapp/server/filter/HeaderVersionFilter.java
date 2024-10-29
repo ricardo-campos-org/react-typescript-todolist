787bcd17739564037078dd42e1d02be92e9c922d
Ricardo Campos <ricardompcampos@gmail.com>
2024-10-29 18:55:24 -0300

    feat: server versions bump (#129)
    * build(deps): bump com.puppycrawl.tools:checkstyle in /server
    
    Bumps [com.puppycrawl.tools:checkstyle](https://github.com/checkstyle/checkstyle) from 10.18.2 to 10.19.0.
    - [Release notes](https://github.com/checkstyle/checkstyle/releases)
    - [Commits](https://github.com/checkstyle/checkstyle/compare/checkstyle-10.18.2...checkstyle-10.19.0)
    
    ---
    updated-dependencies:
    - dependency-name: com.puppycrawl.tools:checkstyle
      dependency-type: direct:production
      update-type: version-update:semver-minor
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * build(deps): bump org.apache.maven.plugins:maven-checkstyle-plugin
    
    Bumps [org.apache.maven.plugins:maven-checkstyle-plugin](https://github.com/apache/maven-checkstyle-plugin) from 3.5.0 to 3.6.0.
    - [Commits](https://github.com/apache/maven-checkstyle-plugin/compare/maven-checkstyle-plugin-3.5.0...maven-checkstyle-plugin-3.6.0)
    
    ---
    updated-dependencies:
    - dependency-name: org.apache.maven.plugins:maven-checkstyle-plugin
      dependency-type: direct:production
      update-type: version-update:semver-minor
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * build(deps): bump org.springframework.boot:spring-boot-starter-parent
    
    Bumps [org.springframework.boot:spring-boot-starter-parent](https://github.com/spring-projects/spring-boot) from 3.3.4 to 3.3.5.
    - [Release notes](https://github.com/spring-projects/spring-boot/releases)
    - [Commits](https://github.com/spring-projects/spring-boot/compare/v3.3.4...v3.3.5)
    
    ---
    updated-dependencies:
    - dependency-name: org.springframework.boot:spring-boot-starter-parent
      dependency-type: direct:production
      update-type: version-update:semver-patch
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * feat: add backend version to all responses in a header
    
    issue #105
    
    ---------
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

---
+++ server/src/main/java/br/com/tasknoteapp/server/filter/HeaderVersionFilter.java
@@ -0,0 +1,31 @@
+package br.com.tasknoteapp.server.filter;
+
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.lang.NonNull;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/** This class manages all the requests adding a custom header with the api version */
+@Component
+public class HeaderVersionFilter extends OncePerRequestFilter {
+
+  @Value("${br.com.tasknote.server.version}")
+  private String apiBuildInfo;
+
+  @Override
+  protected void doFilterInternal(
+    @NonNull HttpServletRequest request,
+    @NonNull HttpServletResponse response,
+    @NonNull FilterChain filterChain)
+    throws ServletException, IOException {
+      response.setHeader("X-BUILD-INFO", apiBuildInfo);
+      filterChain.doFilter(request, response);
+  }
+}
