2fa0c0e4f1d4b910313bb6b4475522404c5de7ac
Ricardo Campos <ricardompcampos@gmail.com>
2024-11-30 13:00:32 -0300

    test: update test cases with new spring 3.4.0 changes

--- server/src/test/java/br/com/tasknoteapp/server/controller/AuthenticationControllerTest.java
+++ server/src/test/java/br/com/tasknoteapp/server/controller/AuthenticationControllerTest.java
@@ -15,8 +15,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
@@ -25,7 +25,7 @@ class AuthenticationControllerTest {
 
   @Autowired private MockMvc mockMvc;
 
-  @MockBean private AuthService authService;
+  @MockitoBean private AuthService authService;
 
   @Test
   @DisplayName("Sign up happy path should succeed")
--- server/src/test/java/br/com/tasknoteapp/server/controller/HomeControllerTest.java
+++ server/src/test/java/br/com/tasknoteapp/server/controller/HomeControllerTest.java
@@ -19,9 +19,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
@@ -30,7 +30,7 @@ class HomeControllerTest {
 
   @Autowired private MockMvc mockMvc;
 
-  @MockBean private HomeService homeService;
+  @MockitoBean private HomeService homeService;
 
   @Test
   @DisplayName("Get summary happy path should succeed")
