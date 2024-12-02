e8f564a3e69ccb8f8bf2a0693518e81943734542
Ricardo Campos <ricardompcampos@gmail.com>
2024-12-02 09:03:28 -0300

    Revert "test: update test cases with new spring 3.4.0 changes"
    This reverts commit 2fa0c0e4f1d4b910313bb6b4475522404c5de7ac.

--- server/src/test/java/br/com/tasknoteapp/server/controller/UserSessionControllerTest.java
+++ server/src/test/java/br/com/tasknoteapp/server/controller/UserSessionControllerTest.java
@@ -13,9 +13,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
@@ -24,7 +24,7 @@ class UserSessionControllerTest {
 
   @Autowired private MockMvc mockMvc;
 
-  @MockitoBean private UserSessionService userSessionService;
+  @MockBean private UserSessionService userSessionService;
 
   @Test
   @DisplayName("Refresh happy path should succeed")
