b56abdc46c951f61f29579fb8d861f5757187ae0
Ricardo Campos <ricardompcampos@gmail.com>
2025-02-18 09:31:20 -0300

    chore: address sonar issues

--- client/src/components/TaskTimeLeft/index.tsx
+++ client/src/components/TaskTimeLeft/index.tsx
@@ -10,19 +10,21 @@ interface Props {
  * Renders the TaskTimeLeft component if the task is not done, displaying
  * a calendar icon and the time left for the task.
  *
- * @param {Props.done} props.done - Boolean value indicating if the task is done. 
- * @param {Props.text} props.text - The time left for the task. 
- * @returns 
+ * @param {Props.done} props.done - Boolean value indicating if the task is done.
+ * @param {Props.text} props.text - The time left for the task.
+ * @returns
  */
 function TaskTimeLeft(props: Props): React.ReactNode | null {
-  return props.done? null : (
-    <div className="d-block">
-      <CalendarCheck />
-      <span className="ms-2 poppins-regular">
-        {props.text}
-      </span>
-    </div>
-  );
+  return props.done
+    ? null
+    : (
+        <div className="d-block">
+          <CalendarCheck />
+          <span className="ms-2 poppins-regular">
+            {props.text}
+          </span>
+        </div>
+      );
 }
 
 export default TaskTimeLeft;
