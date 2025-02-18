d6442cdb7b578cf9b82ac78442356363a1a85729
Ricardo Campos <ricardompcampos@gmail.com>
2025-02-18 09:14:24 -0300

    test: add taskTimeLeft test case

--- client/src/components/TaskTimeLeft/index.tsx
+++ client/src/components/TaskTimeLeft/index.tsx
@@ -6,12 +6,16 @@ interface Props {
   done: boolean;
 }
 
+/**
+ * Renders the TaskTimeLeft component if the task is not done, displaying
+ * a calendar icon and the time left for the task.
+ *
+ * @param {Props.done} props.done - Boolean value indicating if the task is done. 
+ * @param {Props.text} props.text - The time left for the task. 
+ * @returns 
+ */
 function TaskTimeLeft(props: Props): React.ReactNode | null {
-  if (props.done) {
-    return null;
-  }
-
-  return (
+  return props.done? null : (
     <div className="d-block">
       <CalendarCheck />
       <span className="ms-2 poppins-regular">
