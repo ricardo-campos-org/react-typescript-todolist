6fa884d9e52073c70affc62b1bff497fa5f28777
Ricardo Campos <ricardompcampos@gmail.com>
2025-03-09 11:04:33 -0300

    test: add noteTitle component test case

---
+++ client/src/__test__/components/NoteTitle.test.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import { describe, expect, it, vi } from 'vitest';
+import NoteTitle from '../../components/NoteTitle';
+
+describe('NoteTitle test cases', () => {
+  it('should render the note title', () => {
+    const { getByText } = render(<NoteTitle title="Test Title" />)
+
+    expect(getByText('Test Title')).toBeDefined();
+  });
+});
